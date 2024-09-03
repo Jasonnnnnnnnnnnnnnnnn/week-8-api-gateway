2d16f1689ac4ac6ef18d6fd92750f2d18d435d32
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-09-04 03:29:04 +0700

    setting app controller sum2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import axios from 'axios';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService , @Inject('MATH_SERVICE') private client: ClientProxy,) {
+    
+   }
 
   @Get()
   getHello(): string {
@@ -22,7 +25,18 @@ export class AppController {
       console.error('Error calling the microservice', error);
       throw error;
     }
-    
-    
+  }
+
+  @Post('sum2')
+  async sum2(@Body('numbers') numbers: number[]): Promise<number> {
+    try {
+      const response = await axios.post('http://localhost:3001/math/sum', { 
+        numbers,
+       });
+       return response.data;
+    } catch(error) {
+      console.error('Error calling the microservice', error);
+      throw error;
+    }
   }
 }
